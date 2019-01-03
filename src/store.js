51ea4c85fca2a46397d425a6317d2e6c0a6e3849
Hiroki Furuichi <aivvinna@gmail.com>
2019-01-04 02:35:10 +0900

    getting posts from gql now stores in vuex state

--- src/store.js
+++ src/store.js
@@ -1,16 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { gql } from 'apollo-boost'
+import { defaultClient as apolloClient } from './main'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    posts: []
   },
   mutations: {
-
+    setPosts: (state, payload) => {
+      state.posts = payload
+    }
   },
   actions: {
-
+    getPosts: ({ commit }) => {
+      apolloClient.query({
+        query: gql`query {
+          posts {
+            id
+            title
+            body
+          }
+        }`
+      }).then(({data}) => {
+        commit('setPosts', data.posts)
+        console.log('data', data)
+      }).catch(err => {
+        console.error(err)
+      })
+    }
+  },
+  getters: {
+    posts: state => state.posts
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,33 @@
 <template>
   <v-container>
     <h1>HOME</h1>
-    <div v-if="$apollo.loading">Loading...</div>
-    <h2 v-else v-for="post in posts" :key="post.id">{{post.title}} {{post.body}}</h2>
+    <!-- <div v-if="$apollo.loading">Loading...</div> -->
+    <h2 v-for="post in posts" :key="post.id">{{post.title}} {{post.body}}</h2>
   </v-container>
 </template>
 
 <script>
-import gql from 'graphql-tag'
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
 
-  export default {
-    components: {
-      
-    },
-    data() {
-      return {
-        posts: []
-      }
-    },
-    apollo: {
-      posts: {
-        query: gql`query {
-          posts {
-            id
-            title
-            body
-          }
-        }`,
-        result() {
-          console.log(this.$apollo.store)
-        }
-      }
+    }
+  },
+  computed: {
+    posts() {
+      return this.$store.getters.posts
+    }
+  },
+  created() {
+    this.handleGetPosts()
+  },
+  methods: {
+    handleGetPosts() {
+      this.$store.dispatch('getPosts')
     }
   }
+}
 </script>
