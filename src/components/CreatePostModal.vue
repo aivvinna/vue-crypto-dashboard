2051b23fd446e06242dfa7240bd22c0fe7250ce5
Hiroki Furuichi <aivvinna@gmail.com>
2019-02-03 17:01:56 +0900

    fixed translucent create post modal

--- src/components/CreatePostModal.vue
+++ src/components/CreatePostModal.vue
@@ -1,56 +1,59 @@
 <template>
   <v-dialog dark max-width="600" v-model="showCreatePostDialog">
-    <!-- Create Post Title -->
-    <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3>
-        <h1 class="primary--text">Create Post</h1>
-      </v-flex>
-    </v-layout>
+    <v-card>
+      <!-- Create Post Title -->
+      <v-layout row wrap>
+        <v-flex xs12 sm6 offset-sm3>
+          <h1 class="primary--text">Create Post</h1>
+        </v-flex>
+      </v-layout>
 
-    <!-- Create Post Form -->
-    <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3>
+      <!-- Create Post Form -->
+      <v-layout row wrap>
+        <v-flex xs12 sm6 offset-sm3>
 
-        <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="handleCreatePost">
+          <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="handleCreatePost">
 
-          <!-- Title Input -->
-          <v-layout row>
-            <v-flex xs12>
-              <v-text-field :rules="titleRules" v-model="title" label="Post Title" type="text" required></v-text-field>
-            </v-flex>
-          </v-layout>
+            <!-- Title Input -->
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field :rules="titleRules" v-model="title" label="Post Title" type="text" required></v-text-field>
+              </v-flex>
+            </v-layout>
 
-          <!-- Body Text Area -->
-          <v-layout row>
-            <v-flex xs12>
-              <v-textarea :rules="bodyRules" v-model="body" label="Body" type="text" required></v-textarea>
-            </v-flex>
-          </v-layout>
+            <!-- Body Text Area -->
+            <v-layout row>
+              <v-flex xs12>
+                <v-textarea :rules="bodyRules" v-model="body" label="Body" type="text" required></v-textarea>
+              </v-flex>
+            </v-layout>
 
-          <!-- Published Checkbox -->
-          <v-layout row>
-            <v-flex xs12>
-              <v-checkbox
-                :label="`Check to publish`"
-                v-model="published"
-              ></v-checkbox>
-            </v-flex>
-          </v-layout>
+            <!-- Published Checkbox -->
+            <v-layout row>
+              <v-flex xs12>
+                <v-checkbox
+                  :label="`Check to publish`"
+                  v-model="published"
+                ></v-checkbox>
+              </v-flex>
+            </v-layout>
 
-          <v-layout row>
-            <v-flex xs12>
-              <v-btn :loading="loading" :disabled="!isFormValid || loading" color="info" type="submit">
-                <span slot="loader" class="custom-loader">
-                  <v-icon light>cached</v-icon>
-                </span>
-                Submit</v-btn>
-            </v-flex>
-          </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <v-btn :loading="loading" :disabled="!isFormValid || loading" color="info" type="submit">
+                  <span slot="loader" class="custom-loader">
+                    <v-icon light>cached</v-icon>
+                  </span>
+                  Submit</v-btn>
+              </v-flex>
+            </v-layout>
 
-        </v-form>
+          </v-form>
 
-      </v-flex>
-    </v-layout>
+        </v-flex>
+      </v-layout>
+
+    </v-card>
 
   </v-dialog>
 
